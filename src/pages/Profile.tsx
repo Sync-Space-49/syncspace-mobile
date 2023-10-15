260d8558ff0841e48503d3d2ba709ac417bf42b5
sumiverma <sumistargazer@gmail.com>
2023-10-15 15:28:35 -0400

    created base profile page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,15 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Profile.css';
+import {
+  IonContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+// import ExploreContainer from '../components/ExploreContainer';
+import "./Profile.css";
 
 const Profile: React.FC = () => {
   return (
@@ -10,13 +19,53 @@ const Profile: React.FC = () => {
           <IonTitle>Profile</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen color="light">
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Profile" />
+
+        {/* User Profile Pic */}
+        <div className="profile-image-container">
+          <img
+            src="/icons/SyncSpace-mint.png"
+            alt="user pfp"
+            className="profile-image"
+          />
+        </div>
+
+        {/* User Information */}
+        <div className="user-name">Johnny Appleseed</div>
+        <div className="user-at">@johnnyp</div>
+        <div className="user-at">
+          <small>Member since January 2023</small>
+        </div>
+
+        {/* User Organizations*/}
+        <div className="list-title">Your Organizations</div>
+        <IonList inset={true}>
+          <IonItem>
+            <IonLabel>Johnny's Organization</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>PPUX Team Planning</IonLabel>
+          </IonItem>
+        </IonList>
+
+        {/* User Actions */}
+        <div className="list-title">Actions</div>
+        <IonList inset={true}>
+          <IonItem>
+            <IonLabel>Change Password</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Log Out</IonLabel>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Delete Account</IonLabel>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
