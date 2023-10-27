10d83c128f52ba98e31730b73c9d03c5f2696f60
sumiv <62181434+sumiv@users.noreply.github.com>
2023-10-26 20:15:07 -0400

    added org creation with button (#49)

--- src/pages/MyOrgs.tsx
+++ src/pages/MyOrgs.tsx
@@ -7,10 +7,16 @@ import {
   IonSearchbar,
   IonButton,
 } from "@ionic/react";
+import { useState } from "react";
 import "./MyOrgs.css";
 import CustomList from "../components/CustomList";
 
 const MyOrgs: React.FC = () => {
+  const [showCustomList, setShowCustomList] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowCustomList(!showCustomList);
+  };
   return (
     <IonPage>
       <IonHeader>
@@ -27,29 +33,33 @@ const MyOrgs: React.FC = () => {
         <IonSearchbar></IonSearchbar>
 
         <CustomList
-          title="SyncSpace's Boards"
-          titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
-          items={[
-            { text: "Frontend" },
-            { text: "Mobile" },
-            { text: "Backend" },
-          ]}
-        />
-
-        <CustomList
           title="My Boards"
           titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
           items={[{ text: "Laundry Room remodel" }, { text: "Garden" }]}
         />
 
-        <CustomList
+        {showCustomList && (
+          <CustomList
+            title="SyncSpace's Boards"
+            titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
+            items={[
+              { text: "Frontend" },
+              { text: "Mobile" },
+              { text: "Backend" },
+            ]}
+          />
+        )}
+
+        {/* <CustomList
           title="ACM-W's Boards"
           titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
           items={[{ text: "Axe-Hacks" }, { text: "Fall '23" }]}
-        />
+        /> */}
+
         <div className="button">
-          {/* Does having this make sense? Don't we already have a '+' button at the very top? */}
-          <IonButton shape="round">+ Add Organization</IonButton>
+          <IonButton shape="round" onClick={handleButtonClick}>
+            + Add Organization
+          </IonButton>
         </div>
       </IonContent>
     </IonPage>
