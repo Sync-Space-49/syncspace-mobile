c8b646c45bb54ffefd4f5db8d21e890e69d11f22
sumiv <62181434+sumiv@users.noreply.github.com>
2023-11-05 21:14:49 -0500

    added create new organization button (#58)

--- src/pages/MyOrgs.tsx
+++ src/pages/MyOrgs.tsx
@@ -6,17 +6,36 @@ import {
   IonToolbar,
   IonSearchbar,
   IonButton,
+  IonButtons,
+  IonPopover,
+  IonIcon,
 } from "@ionic/react";
 import { useState } from "react";
 import "./MyOrgs.css";
 import CustomList from "../components/CustomList";
+import { addOutline } from "ionicons/icons";
+import { useHistory } from "react-router";
 
 const MyOrgs: React.FC = () => {
-  const [showCustomList, setShowCustomList] = useState(false);
+  // const [showCustomList, setShowCustomList] = useState(false);
+  const [popoverState, setPopoverState] = useState<{
+    showPopover: boolean;
+    event: Event | undefined;
+  }>({ showPopover: false, event: undefined });
 
-  const handleButtonClick = () => {
-    setShowCustomList(!showCustomList);
+  const handleButtonClick = (e: React.MouseEvent) => {
+    e.persist();
+    setPopoverState({ showPopover: true, event: e.nativeEvent });
   };
+
+  const history = useHistory();
+
+  const handleCreateOrganization = () => {
+    console.log("org and board created");
+    history.push("/app/organization");
+    setPopoverState({ showPopover: false, event: undefined });
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -28,6 +47,27 @@ const MyOrgs: React.FC = () => {
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">My Organizations</IonTitle>
+            <IonButtons slot="end">
+              <IonButton onClick={handleButtonClick} className="add-btn">
+                <IonIcon slot="icon-only" icon={addOutline} />
+              </IonButton>
+            </IonButtons>
+            <IonPopover
+              isOpen={popoverState.showPopover}
+              event={popoverState.event}
+              onDidDismiss={() =>
+                setPopoverState({ showPopover: false, event: undefined })
+              }
+            >
+              <IonContent class="ion-padding">
+                <button
+                  onClick={handleCreateOrganization}
+                  className="create-org-btn"
+                >
+                  Create a New Organization
+                </button>
+              </IonContent>
+            </IonPopover>
           </IonToolbar>
         </IonHeader>
         <IonSearchbar></IonSearchbar>
@@ -38,29 +78,21 @@ const MyOrgs: React.FC = () => {
           items={[{ text: "Laundry Room remodel" }, { text: "Garden" }]}
         />
 
-        {showCustomList && (
-          <CustomList
-            title="SyncSpace's Boards"
-            titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
-            items={[
-              { text: "Frontend" },
-              { text: "Mobile" },
-              { text: "Backend" },
-            ]}
-          />
-        )}
-
-        {/* <CustomList
+        <CustomList
           title="ACM-W's Boards"
           titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
           items={[{ text: "Axe-Hacks" }, { text: "Fall '23" }]}
-        /> */}
+        />
 
-        <div className="button">
-          <IonButton shape="round" onClick={handleButtonClick}>
-            + Add Organization
-          </IonButton>
-        </div>
+        <CustomList
+          title="SyncSpace's Boards"
+          titleImg="https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png"
+          items={[
+            { text: "Frontend" },
+            { text: "Mobile" },
+            { text: "Backend" },
+          ]}
+        />
       </IonContent>
     </IonPage>
   );
