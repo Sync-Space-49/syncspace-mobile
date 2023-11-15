513163cfb052cab58f88504654388a80cb66c872
tylerminnis <tylerminnis10@outlook.com>
2023-11-15 14:37:05 -0500

    added delete account functionality, be carefulgit add . , also cleaned up some stuff

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -7,25 +7,29 @@ import {
   IonPage,
   IonTitle,
   IonToolbar,
-  IonLoading,
   IonAvatar,
-  IonAlert,
-  IonButton,
 } from "@ionic/react";
 import "./Profile.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import LogoutButton from "../components/Logout";
 import DeleteButton from "../components/DeleteAccount";
-import { domain, serverAdress, serverAudience } from "../auth.config";
+import { serverAdress } from "../auth.config";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Organization } from "../types";
+import CustomList from "../components/CustomList";
+
+interface ListProps {
+  text: string; // the list's content
+  listImg?: string; //list image src
+}
 
 const Profile: React.FC = () => {
   const { user, getAccessTokenSilently } = useAuth0();
 
   const [organizations, setOrganizations] = useState<Organization[]>([]);
-  const [userSince, setUserSince] = useState();
+  const [orgNames, setOrgNames] = useState<ListProps[]>([]);
+  const [userSince, setUserSince] = useState<string>('October 2023');
 
   const getOrgs = async () => {
     let token = await getAccessTokenSilently();
@@ -46,34 +50,42 @@ const Profile: React.FC = () => {
       })
   }
 
-  const getUserSince = async () => {
-    let token = await getAccessTokenSilently();
-    let userId = user!.sub;
-    const options = {
-      method: "GET",
-      url: `https://${domain}/api/v2/users/${userId}`,
-      headers: { authorization: `Bearer ${token}` },
-    }
-    await axios(options)
-      .then((response) => {
-        let data = response.data;
-        setUserSince(data);
+  // to be updated with getting userSince data
 
-      })
-      .catch((error) => {
-        console.error(error.message);
-      })
-  }
+  // const getUserSince = async () => {
+  //   let token = await getAccessTokenSilently();
+  //   let userId = user!.sub;
+  //   const options = {
+  //     method: "GET",
+  //     url: `https://${domain}/api/v2/users/${userId}`,
+  //     headers: { authorization: `Bearer ${token}` },
+  //   }
+  //   await axios(options)
+  //     .then((response) => {
+  //       let data = response.data;
+  //       setUserSince(data);
+
+  //     })
+  //     .catch((error) => {
+  //       console.error(error.message);
+  //     })
+  // }
 
   useEffect(() => {
-    getUserSince();
+    // getUserSince();
     getOrgs();
-  },[]);
+  }, []);
 
-  // useEffect(() => {
-  //   setOrganizations(data);
-  //   setUserSince(data);
-  // },[]); 
+  useEffect(() => {
+    if (organizations.length > 0) {
+      const updatedOrgNames = organizations.map((org) => ({
+        text: org.name,
+        listImg: "https://s3.us-east-1.wasabisys.com/sync-space/logo/SyncSpace-mint.png",
+      }));
+      setOrgNames(updatedOrgNames);
+    }
+    // setUserSince(data);
+  }, [organizations]);
 
   return (
     <IonPage>
@@ -88,48 +100,26 @@ const Profile: React.FC = () => {
             <IonTitle size="large">Profile</IonTitle>
           </IonToolbar>
         </IonHeader>
-
-        {/* User Profile Pic */}
         <div className="profile-image-container">
           <IonAvatar className="avatar">
             <img src={user?.picture} alt={user?.name} />
           </IonAvatar>
         </div>
-
-        {/* User Information */}
-        {/* <div className="user-name">Johnny Appleseed</div> */}
-        {/* <div className="user-at">@johnnyp</div> */}
-
         <div className="user-name">{user?.name}</div>
         <div className="user-at">{user?.nickname}</div>
-
         <div className="user-at">
+          {/* replace with created_at */}
           <small>Member since {userSince}</small>
-          <small>Member since {user?.created_at}</small>
         </div>
-
-        {/* User Organizations*/}
         <div className="list-title">Your Organizations</div>
-        <IonList inset={true}>
-          <IonItem>
-            <IonLabel>Johnny's Organization</IonLabel>
-          </IonItem>
-          <IonItem>
-            <IonLabel>PPUX Team Planning</IonLabel>
-          </IonItem>
-        </IonList>
-
-        {/* User Actions */}
+        <CustomList items={orgNames} />
         <div className="list-title">Actions</div>
         <IonList inset={true}>
           <IonItem>
             <IonLabel>Change Password</IonLabel>
           </IonItem>
           <LogoutButton />
-          {/* <IonItem> */}
-          {/* <IonLabel>Delete Account</IonLabel> */}
           <DeleteButton />
-          {/* </IonItem> */}
         </IonList>
       </IonContent>
     </IonPage>
