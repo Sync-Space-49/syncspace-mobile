5d4e90791f74e9d7e751bf81a2e4393aac5befc1
tylerminnis <tylerminnis10@outlook.com>
2023-11-22 10:59:07 -0500

    added most of board functionality

--- src/components/BoardStack.tsx
+++ src/components/BoardStack.tsx
@@ -10,56 +10,88 @@ import {
   ItemReorderEventDetail,
 } from "@ionic/react";
 import CardSettings from "../components/CardSettings";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { Stack, Card } from "../types";
+// import SpecificCard from "./SpecificCard";
+import "swiper/css";
+import "swiper/css/pagination";
 
 interface Item {
   id: string;
-  label: string;
-}
-interface StackProps {
-  title: String;
-  items: Item[];
-  aiDisabled?: Boolean;
+  title: string;
 }
 
-const BoardStack: React.FC<StackProps> = ({ title, items, aiDisabled }) => {
+interface StackProps {
+  stack: Stack;
+  updateStackList?: () => void;
+}
+
+const BoardStack: React.FC<StackProps> = ({ stack }) => {
+
   const [selectedItem, setSelectedItem] = useState<Item | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [cards, setCards] = useState<Card[]>([]);
+  const [currentCard, setCurrentCard] = useState<Card>();
+  const [title, setTitle] = useState<String>();
 
-  const handleItemClick = (item: Item) => {
-    setSelectedItem(item);
+  // modal vars
+  const modal = useRef<HTMLIonModalElement>(null);
+  const page = useRef(undefined);
+
+  const handleItemClick = (card: Card) => {
+    console.log('clicked');
+    // console.log(item);
+    setCurrentCard(card)
+    setSelectedItem(card);
     setIsModalOpen(true);
   };
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedItem(null);
-  };
-
+  // need to update with position in server
   function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
     console.log("dragged from index", event.detail.from, "to", event.detail.to);
     event.detail.complete();
   }
+
+  useEffect(() => {
+    if (stack) {
+      setTitle(stack.title);
+      if (stack.cards.length > 0) {
+        setCards(stack.cards);
+      }
+    }
+  }, [stack]);
+
+  useEffect(() => {
+    console.log(isModalOpen);
+  }, [isModalOpen])
 
   return (
     <IonCard className="ion-padding">
       <IonCardTitle>{title}</IonCardTitle>
       <IonCardContent>
         <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
-          {items.map((item) => (
-            <IonItem key={item.id} onClick={() => handleItemClick(item)}>
-              <IonLabel>{item.label}</IonLabel>
-              <IonReorder slot="end"></IonReorder>
+          {cards && cards.length > 0 ? (
+            cards.map((card: Card, i) => (
+              <IonItem key={card.id} onClick={() => handleItemClick(card)}>
+                <IonLabel>{card.title}</IonLabel>
+                <IonReorder slot="end"></IonReorder>
+              </IonItem>
+            ))
+          ) : (
+            <IonItem>
+              <IonLabel>This stack has no cards</IonLabel>
             </IonItem>
-          ))}
+          )}
         </IonReorderGroup>
       </IonCardContent>
       {/* Modal */}
       {selectedItem && (
         <CardSettings
-          title={selectedItem.label}
+          key={currentCard?.id}
+          card={currentCard!}
           isOpen={isModalOpen}
-          onClose={closeModal}
+          onClose={() => setIsModalOpen(false)}
+          onOpenChange={(isOpen) => setIsModalOpen(isOpen)}
         />
       )}
     </IonCard>
