eb1c8d3d0f69c0021047dab0fa894213534733f8
DylanHalstead <dylanhalstead11@gmail.com>
2023-12-07 14:30:34 -0500

    testing different redirect uri

--- src/main.tsx
+++ src/main.tsx
@@ -18,7 +18,7 @@ root.render(
     domain={auth0Domain}
     clientId={clientId}
     authorizationParams={{
-      redirect_uri: callbackUri,
+      redirect_uri: window.location.origin,
       audience: serverAudience,
     }}
     useRefreshTokens={true}
